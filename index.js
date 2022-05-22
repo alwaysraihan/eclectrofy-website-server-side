1002d4335bba9a0c4d29cd6ad3ec9546b3dc2e13
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-05-22 16:18:19 +0600

    find data by id and send data api added

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
+
+const cors = require("cors");
+const app = express();
+const jwt = require("jsonwebtoken");
+app.use(cors());
+require("dotenv").config();
+app.use(express.json());
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tllku.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log("Database Connected");
+        const toolsCoolection = client.db("Menufacturer").collection("Tools");
+        app.get("/tools", async (req, res) => {
+            const qurey = {};
+            const cursor = toolsCoolection.find(qurey);
+            const tools = await cursor.limit(6).toArray();
+
+            res.send(tools);
+        });
+        app.get("/tools/:id", async (req, res) => {
+            const id = req.params.id;
+
+            try {
+                const qurey = { _id: ObjectId(id) };
+                const tools = await toolsCoolection.findOne(qurey);
+                res.send(tools);
+            } catch (error) {
+                return res.send({ message: "Data not found" });
+            }
+        });
+    } finally {
+    }
+};
+run().catch(console.dir);
+app.get("/", (reqest, res) => {
+    res.status(200).send("Hello I'm set and listening form doctor portal");
+});
+
+app.listen(port, () => {
+    console.log("Listening on port", port);
+});
