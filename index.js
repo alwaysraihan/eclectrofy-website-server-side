fb82baa903e04c25a7879edc83473cd96dd307aa
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-05-25 18:06:19 +0600

    get all users api added

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const { ObjectId } = require("mongodb");
 
 const cors = require("cors");
-const app = express();
-const jwt = require("jsonwebtoken");
-app.use(cors());
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const app = express();
+app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
@@ -17,20 +18,123 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1,
 });
 
+const verifyToken = async (req, res, next) => {
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+        return res.status(401).send({ message: "UnAutorized access" });
+    }
+    const authToken = authorizationHeader.split(" ")[1];
+    jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+};
+
 const run = async () => {
     try {
         await client.connect();
         console.log("Database Connected");
         const toolsCoolection = client.db("Menufacturer").collection("Tools");
         const orderCollection = client.db("Menufacturer").collection("Orders");
+        const UserCollections = client.db("Menufacturer").collection("Users");
+        const PaymentCollections = client
+            .db("Menufacturer")
+            .collection("payment");
+
+        // verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await UserCollections.findOne({
+                email: requester,
+            });
+            if (requesterAccount.role === "admin") {
+                next();
+            } else {
+                return res.status(403).send({ message: "Forbidden access" });
+            }
+        };
+        /* Admin section
+        1. get admin
+        2. put admin
+        
+        */
+        // get  admin
+        app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = await UserCollections.findOne({ email: email });
+            const isAdmin = user.role === "admin";
+            res.send({ admin: isAdmin });
+        });
+        // put admin
+        app.put(
+            "/user/admin/:email",
+            verifyToken,
+            verifyAdmin,
+            async (req, res) => {
+                const email = req.params.email;
+
+                const filter = { email: email };
+
+                const updateDoc = {
+                    $set: { role: "admin" },
+                };
+                const result = await UserCollections.updateOne(
+                    filter,
+                    updateDoc
+                );
+
+                return res.send({ result });
+            }
+        );
+        // user
+        app.get("/user", verifyToken, async (req, res) => {
+            const user = await UserCollections.find().toArray();
+            res.send(user);
+        });
+
+        app.post("/create-payment-intent", verifyToken, async (req, res) => {
+            const order = req.body;
+
+            const subTotal = order.subTotal;
+            const amount = subTotal * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+            res.status(200).send({ clientSecret: paymentIntent.client_secret });
+        });
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await UserCollections.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.send({ result, token });
+        });
         app.get("/tools", async (req, res) => {
             const qurey = {};
             const cursor = toolsCoolection.find(qurey);
-            const tools = await cursor.limit(6).toArray();
+            const tools = await cursor.toArray();
 
             res.send(tools);
         });
-        app.get("/tools/:id", async (req, res) => {
+        app.get("/tools/:id", verifyToken, async (req, res) => {
             const id = req.params.id;
 
             try {
@@ -41,7 +145,12 @@ const run = async () => {
                 return res.send({ message: "Data not found" });
             }
         });
-        app.get("/order", async (req, res) => {
+        app.post("/tools", verifyToken, verifyAdmin, async (req, res) => {
+            const product = req.body;
+            const result = await toolsCoolection.insertOne(product);
+            res.send(result);
+        });
+        app.get("/order", verifyToken, async (req, res) => {
             const email = req.query.email;
             try {
                 const qurey = { buyerEmail: email };
@@ -51,10 +160,46 @@ const run = async () => {
                 return res.send({ message: "Data not found" });
             }
         });
+        app.get("/order/:id", verifyToken, async (req, res) => {
+            const id = req.params.id;
+
+            try {
+                const qurey = { _id: ObjectId(id) };
+                const order = await orderCollection.findOne(qurey);
+                res.send(order);
+            } catch (error) {
+                return res.send({ message: "Data not found" });
+            }
+        });
         app.post("/order", async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send({ success: true, result });
+        });
+        app.patch("/order/:id", verifyToken, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            try {
+                const filter = { _id: ObjectId(id) };
+
+                const options = { upsert: true };
+                const updateDoc = {
+                    $set: {
+                        paid: true,
+                        transactionId: payment.transactionId,
+                    },
+                };
+                const result = await orderCollection.updateOne(
+                    filter,
+                    updateDoc
+                );
+                const updatePayment = await PaymentCollections.insertOne(
+                    payment
+                );
+                res.send(result);
+            } catch (error) {
+                return res.send({ message: "Data not found" });
+            }
         });
     } finally {
     }
